aba2600f0f1eac2c7456c8147f59d00bd0b08880
retoLabs <info@reto-labs.es>
2022-05-12 11:58:18 +0200

    Topol MALLA +|- OK

--- kernel/libK1_Topol.js
+++ kernel/libK1_Topol.js
@@ -43,8 +43,8 @@
 		Nota: 123 es un número escogido arbitrariamente. 
 		Diferencia {id0_calc} entre: Arcos A-->B, y Arcos B-->A
 
-	Un NUDO es un Arco, que representa la relación entr dos Nodos en una Malla
-	Siempre representa un arco de un nodo ROW a un nodo COL
+	Un ARCO es un Arco, que representa la relación entr dos Nodos en una Malla
+	Siempre representa un arco de un nodo NROW a un nodo NCOL
 	Consta de:
 		+ {valor} : es el valor que aparecerá en la celda de la tabla
 
@@ -57,7 +57,7 @@
 		Lista : Nodos con un numeral de orden {num}
 		Arbol : Nodos con {id1} que apunta al {id0} del nodo padre
 		Grafo : [Nodos | Drags] + Arcos (Nodos con rol = 'NODO', Arcos con rol = 'ARCO')
-		Malla : Nodos + Nudos (Nodos con rol = ['ROW'|'COL'], Nudos con rol = 'NUDO')
+		Malla : Nodos + arco (Nodos con rol = ['NROW'|'NCOL'], arco con rol = 'ARCO')
 
 	Cada topología puede tener métodos para añadir propiedades de forma
 	que se optimize el manejo de los nodos.
@@ -214,18 +214,18 @@ class rArco {
 	}
 }
 
-//------------------------------------------------------------------- Class Nudo (de Malla)
-class rNudo  extends rArco{
-	constructor(tag,nRow,nCol,valor){
-		super(tag,nRow,nCol);
-		this.iam = 'rNudo';
-		this.rol = 'NUDO';
+//------------------------------------------------------------------- Class ARCO (de Malla)
+class rARCO  extends rArco{
+	constructor(tag,nNROW,nNCOL,valor){
+		super(tag,nNROW,nNCOL);
+		this.iam = 'rARCO';
+		this.rol = 'ARCO';
 		this.valor = valor;
 	}
 	objDB2Clase(objDB){
 		super.objDB2Clase(objDB);
-		this.iam = 'rNudo';
-		this.rol = 'NUDO';
+		this.iam = 'rARCO';
+		this.rol = 'ARCO';
 		this.valor = objDB.valor;
 		this.id0I = objDB.id0I;
 		this.id0F = objDB.id0F;
@@ -420,7 +420,7 @@ class rArbol extends rTopol {
 		this.optimiza(this.nodos);
 
 	}
-// Crear hijos y poner estados EXPAN | COLAP | FULLA
+// Crear hijos y poner estados EXPAN | NCOLAP | FULLA
 	optimiza (nodos){
 		nodos.map(function(nodo,ix){
 			nodo.stat = 'FULLA';
@@ -549,7 +549,7 @@ class rArbol extends rTopol {
 	}
 
 	commuta(nodo){
-		if (nodo.stat == 'EXPAN') nodo.stat = 'COLAP';
+		if (nodo.stat == 'EXPAN') nodo.stat = 'NCOLAP';
 		else nodo.stat = 'EXPAN';
 	}
 }
@@ -707,8 +707,8 @@ class rGrafo extends rTopol {
 		ixF = this.index.indexOf(id1);
 
 		if (ixI >=0 && ixF >= 0) {
-			arco.nodoI = ixI;
-			arco.nodoF = ixF;
+			arco.ixI = ixI;
+			arco.ixF = ixF;
 			this.arcos.push(arco);
 			this.iArcs.push(arco.id0);
 		}
@@ -768,30 +768,6 @@ class rGrafo extends rTopol {
 	}
 
 }
-
-
-//------------------------------------------------------------------- Class Gantt
-class rGantt extends rGrafo{
-	constructor(nombre,nodos,lapso) {
-		super(nombre,nodos);
-		this.meta.iam = 'rGantt';
-		this.lapso = lapso; // es el periodo representado en la gráfica
-	}
-
-	setLapso(lapso){
-		this.lapso = lapso;
-	}
-
-	getLapso(){
-		return this.lapso;
-	}
-	objDB2Clase(objDB){
-		super.objDB2Clase(objDB); // rGrafo
-		this.meta.iam = 'rGantt';
-	}
-}
-/*
-*/
 //------------------------------------------------------------------- Class Malla
 // Pseudo Grafo. Los arcos van de un conjunto de nodos a otro
 class rMalla extends rTopol{
@@ -800,377 +776,125 @@ class rMalla extends rTopol{
 		this.meta.iam = 'rMalla';
 
 
-		this.mcols = [];
-		this.mrows = [];
-		this.nudos = [];
+		this.ncols = [];
+		this.nrows = [];
+		this.arcos = [];
 
 		this.iCols = [];
 		this.iRows = [];
-		this.iNudos = [];
-
-		this.loTable = '';
+		this.iArcs= [];
 
 		this.separaNodos();
-		this.optimizaNudos()
+		this.optimizaArcos()
 	}
 
 	separaNodos(){
+		this.nrows = [];
+		this.ncols = [];
+		this.arcos = [];
+
+		this.iCols = [];
+		this.iRows = [];
+
+		var roles = ['NROW','NCOL','ARCO'];
 		this.nodos.map(function(nodo){
-			if (!nodo.rol){console.log('Nodo '+nodo.tag+' sin ROL');}
-			else if (nodo.rol == 'COL') {this.iCols.push(nodo.id0); this.mcols.push(nodo);}
-			else if (nodo.rol == 'ROW') {this.iRows.push(nodo.id0); this.mrows.push(nodo);}
-			else if (nodo.rol == 'NUDO'){this.nudos.push(nodo);}
+			if (!nodo.rol || roles.indexOf(nodo.rol) == -1) console.log('Nodo',nodo.tag,'sin ROL OK');
+			else if (nodo.rol == 'NCOL'){this.ncols.push(nodo); this.iCols.push(nodo.id0);}
+			else if (nodo.rol == 'NROW'){this.nrows.push(nodo); this.iRows.push(nodo.id0);}
+			else if (nodo.rol == 'ARCO'){this.arcos.push(nodo);}
 		}.bind(this))
 	}
 
-	optimizaNudos(){
-		this.iNudos = [];
-		this.nudos.map(function(nudo){
-			var idx = nudo.getId0Real();
-			nudo.ix0 = this.iCols.indexOf(idx);
-			nudo.ix1 = this.iRows.indexOf(nudo.id1);
-			this.iNudos.push(nudo.ix0+':'+nudo.ix1);
+	optimizaArcos(){
+		this.iArcs = [];
+		this.arcos.map(function(arco){
+			var idx = arco.getId0Real();
+			arco.ixI = this.iCols.indexOf(idx);
+			arco.ixF = this.iRows.indexOf(arco.id1);
+			this.iArcs.push(arco.ixI+':'+arco.ixF);
 		}.bind(this))
 	}
 
-	addNodoRow(nodo){
-		nodo.rol = 'ROW';
-		var ix = this.iRows.indexOf(nodo.id0);
-		if (ix == -1){ 
-			this.mrows.push(nodo);
-			this.iRows.push(nodo.id0);
-		}
+	regenera(){
+		var nodos = [];
+		nodos = nodos.concat(this.ncols);
+		nodos = nodos.concat(this.nrows);
+		nodos = nodos.concat(this.arcos);
+		this.nodos = nodos;
+		this.separaNodos();
+		this.optimizaArcos();
 	}
-	addNodoCol(nodo){
-		nodo.rol = 'COL';
-		var ix = this.iCols.indexOf(nodo.id0);
-		if (ix == -1){ 
-			this.mcols.push(nodo);
-			this.iCols.push(nodo.id0);
-		};
+	
+	addNodoSelf(nodo){
+		console.log(utils.o2s(nodo));
+		var roles = ['NROW','NCOL','ARCO'];
+		if (roles.indexOf(nodo.rol) == -1) return;
+		super.addNodo(nodo);
+		this.separaNodos();
+		this.optimizaArcos();
+		console.log(utils.o2s(this.nrows),utils.o2s(this.ncols),utils.o2s(this.arcos));
+
 	}
 
-	addNudo(nudo){
-		this.nudos.push(nudo);
-		this.optimizaNudos();
+	addArcoSelf(arco){
+		this.addNodoSelf(arco);
 	}
-	borraNudoSelf(nudo){
-		var ix0 = nudo.ix0;
-		var ix1 = nudo.ix1;
-		var ixN = this.iNudos.indexOf(ix0+':'+ix1);
-		if (ixN == -1) {alert('Nudo '+ nudo.tag +' no existe!'); return;}
-		this.nudos.splice(ixN,1);
+	getNodosRow(){
+		return this.nrows;
 	}
-	updtNudoSelf(nudo){
-		var ix0 = nudo.ix0;
-		var ix1 = nudo.ix1;
-		var ixN = this.iNudos.indexOf(ix0+':'+ix1);
-		if (ixN == -1) {alert('Nudo '+ nudo.tag +' no existe!'); return;}
-		this.nudos[ixN] = nudo;
+	getRowByIx(ix){
+		return this.nrows[ix];
+	}
+	getNodosCol(){
+		return this.ncols;
+	}
+	getColByIx(ix){
+		return this.ncols[ix];
+	}
+	getArcos(){
+		return this.arcos;
+	}
+
+	getIxFromArco(arco){
+		var ix0 = arco.ix0;
+		var ix1 = arco.ix1;
+		var ixN = this.iArcs.indexOf(ix0+':'+ix1);
+		return ixN;
+	}
+	borraArcoSelf(arco){
+		var ixn = this.getIxFromArco(arco);
+		if (ixN == -1) {console.log('Arco '+ arco.tag +' no existe!'); return;}
+		this.arco.splice(ixN,1);
+		this.regenera();
+	}
+
+	updtArcoSelf(arco){
+		var ixn = this.getIxFromArco(arco);
+		if (ixN == -1) {console.log('Arco '+ arco.tag +' no existe!'); return;}
+		this.arco[ixN] = arco;
+		this.regenera();
 	}
 
 	objDB2Clase(objDB){
 		super.objDB2Clase(objDB);
 		this.meta.iam = 'rMalla';
 		this.separaNodos();
-		this.optimizaNudos();
+		this.optimizaArcos();
 	}
 
 	clase2ObjDB(){
 		var nodos = [];
-		nodos = nodos.concat(this.mcols);
-		nodos = nodos.concat(this.mrows);
-		nodos = nodos.concat(this.nudos);
+		nodos = nodos.concat(this.ncols);
+		nodos = nodos.concat(this.nrows);
+		nodos = nodos.concat(this.arcos);
 		return {"meta": this.meta, "nodos" : nodos};
 	}
 
-	getGridCols(){
-		var gridCols = ['Tag'];
-		var n = this.mrows.length;
-		for (var i=0;i<n;i++){
-			gridCols.push(this.mrows[i].tag)
-		}
-		return gridCols;
-	}
-
-	getGridData(){
-		var nudo = '';
-		var gridData = [];
-		var nx = this.mrows.length;
-		var ny = this.mcols.length;
-		for (var y=0;y<ny;y++){
-			var fila = {};
-			fila['Tag'] = {ix:-(y+1), valor:this.mcols[y].tag};
-			for (var x=0;x<nx;x++){
-				var ixNudo = this.iNudos.indexOf(y+':'+x);
-				if ( ixNudo == -1) fila[this.mrows[x].tag] = 0;
-				else {
-					nudo = this.nudos[ixNudo];
-					fila[this.mrows[x].tag]= {ix:ixNudo+1, valor:nudo.valor};
-				}
-			}
-			gridData.push(fila);
-		}
-		return gridData;
-	}
-
-	setHeaders (tbl){
-		var thead = rEl$('thead');
-		var tr = rEl$('tr');
-		var th0 =rEl$('th');
-		th0.innerHTML = '&nbsp;'
-		th0.style.cssText = 'border:1px solid red';
-		tr.appendChild(th0);
-
-		this.mcols.map(function(col){
-			var th = rEl$('th');
-			th.innerHTML = col.tag;
-			th.style.cssText = 'border:1px solid black';
-			tr.appendChild(th);
-		}.bind(this))
-		thead.appendChild(tr);
-		tbl.appendChild(thead);
-	}
-
-	setCelda(idTD, valor){
-		if (!idTD || !valor ) return;
-		var ixNudo = this.iNudos.indexOf(idTD);
-		if (ixNudo >= 0) {this.nudos[ixNudo].valor = valor;}
-		else {
-			var col = this.mcols[parseInt(idTD.split(':')[0])];
-			var row = this.mrows[parseInt(idTD.split(':')[1])];
-			var nudo = new rNudo(col,row,'Nudo '+idTD,valor);
-			this.nudos.push(nudo);
-			this.iNudos.push(idTD);
-			}
-	}
-
-	creaTDs (row,tr){
-		this.mcols.map(function(col){
-			var td = rEl$('td');
-			td.id = col.num +':'+row.num;
-			td.style.cssText = 'border:1px solid black';
-			var ixNudo = this.iNudos.indexOf(col.num+':'+row.num);
-//			console.log(col.num+':'+row.num+ '->'+ixNudo);
-			if (ixNudo >= 0) td.innerHTML = this.nudos[ixNudo].tag;
-			else td.innerHTML = '&nbsp;';
-
-			td.onclick = function(e){this.setCelda(e.target.id);}.bind(this);
-			tr.appendChild(td);
-
-		}.bind(this))
-	}
-
-	setTBody(tbl){
-		var tBody = rEl$('tbody');
-		this.mrows.map(function(row){
-			var tr = rEl$('tr');
-			var td0 = rEl$('td');
-			td0.style.cssText = 'border:1px solid blue';
-			td0.innerHTML = row.tag;
-			tr.appendChild(td0);
-			this.creaTDs(row,tr);
-			tBody.appendChild(tr);
-		}.bind(this))
-
-		tbl.appendChild(tBody);
-	}
-
-	getTabla(){
-		var tabla = document.createElement('table');
-		this.setHeaders(tabla);
-		this.setTBody(tabla);
-		this.loTable = tabla;
-		return tabla;
-
-	}
-	show (win0,clases,css){
-		win0.show();
-		var divBase = win0.tapa;
-		var taula = rEl$('table');
-//		taula.className = 'table';
-		if (clases) taula.className += clases;
-		if (css) taula.style.cssText = css;
-		this.setHeaders(taula);
-		this.setTBody(taula);
-		divBase.appendChild(taula);
-		this.loTable = taula;
-	}
 }
 
-//------------------------------------------------------------------- Class MallaTree
-// Una MALLA-Tree es un híbrido Arbol/Grafo.
-// Los Nodos pertenecen a dos Listas (Rows Cols)
-// Los NUDOS (~Arcos) conectan dos nodos, uno de cada lista, por su indice {num}
-// La estructura básica se compone de un Arbol, con tres nodos singulares:
-// Raiz : Info global de la Malla
-//  +--- ROWS : Nodos cabecera de fila
-//  +--- COLS : Nodos cabecera de columna
-//  +--- NUDOS: Arcos entre Rows y Cols
-//-------------------------------------------------------------------
-class rMallaTree extends rArbol {
-	constructor(tag,nodos){
-		super(tag,nodos);
-		this.meta.iam = 'rMallaTree';
-		if (nodos.length == 1) this.inicio();
-	}
-
-	inicio(){
-		var raiz = this.getRaiz();
-		var rows = new rNodo('Rows'); rows.rol = 'ROWS';
-		var cols = new rNodo('Cols'); cols.rol = 'COLS';
-		var nudos = new rNodo('Nudos'); nudos.rol = 'NUDOS';
-
-		this.addNodoHijo(raiz,rows);
-		this.addNodoHijo(raiz,cols);
-		this.addNodoHijo(raiz,nudos);
-	}
-	objDB2Clase(objDB){
-		super.objDB2Clase(objDB);
-		this.meta.iam = 'rMallaTree';
-	}
-
-	getNodoByRol(rol){
-		var nodox = null;
-		var raspa = this.getRaspa();
-		raspa.map(function(nodo){
-			if (nodo.rol == rol ) nodox = nodo; 
-		}.bind(this))
-		return nodox;
-	}
-	addNodoRow(nodox){
-		var nodoRows = this.getNodoByRol('ROWS');
-		if (nodoRows && nodoRows.hijos.indexOf(nodox.id0) == -1) {
-			this.addNodoHijo(nodoRows,nodox);
-//			console.log('addNodoRow '+utils.o2s(nodox))
-		}
-		else if (nodoRows && nodoRows.hijos.indexOf(nodox.id0) > -1)
-			console.log ('Nodo Row 2 : '+nodox.tag+' ya existe');
-	}
-
-	getNodosRows(){
-		var rows = [];
-		var nodoRows = this.getNodoByRol('ROWS');
-		if (nodoRows) rows = this.getHijosNodo(nodoRows);
-		return rows;
-	}
-
-	addNodoCol(nodox){
-		var nodoCols = this.getNodoByRol('COLS');
-		if (nodoCols && nodoCols.hijos.indexOf(nodox.id0) == -1) {
-			this.addNodoHijo(nodoCols,nodox);
-//			console.log('addNodoCol '+utils.o2s(nodox))
-		}
-		else if (nodoCols && nodoCols.hijos.indexOf(nodox.id0) > -1)
-			console.log ('Nodo Col '+nodox.tag+' ya existe');
-	}
-
-	getNodosCols(){
-		var cols = [];
-		var nodoCols = this.getNodoByRol('COLS');
-		if (nodoCols) cols = this.getHijosNodo(nodoCols);
-		return cols;
-	}
-
-	addNudo(nudo){
-		nudo.rol = 'NUDO';
-		var nodoNudos = this.getNodoByRol('NUDOS');
-		if (!nodoNudos) return null;
-
-		var iNus = nodoNudos.hijos.indexOf(nudo.id0);
-
-		if (iNus == -1){ 
-			nudo.id0I = nudo.getId0Real();
-			nudo.id0F = nudo.id1; // el id1 actual se 'machaca' al hacer addNodoHijo
-			this.addNodoHijo(nodoNudos,nudo);
-//			console.log('addNudo '+utils.o2s(nudo))
-		}
-		else if (iNus > -1){
-//			console.log ('Nudo '+nudo.tag+' ya existe');
-		}
-		return iNus;
-	}
-
-// Al borrar un nudo, comprueba si los nodos COL y ROW tienen otros nudos que les apunten.
-// Si no es así, se eliminan estos nodos 'inútiles'
-	borraNudo(nudox,onOff){
-		var nodoNudos = this.getNodoByRol('NUDOS');
-		if (nodoNudos && nodoNudos.hijos.indexOf(nudox.id0) == -1){ 
-			console.log ('Nudo '+nudox.tag+' NO existe');
-		}
-		else {
-			var colComun = 0;
-			var rowComun = 0;
-			var nudos = this.getNudos();
-			nudos.map(function(nudo){
-//				console.log('BorraNudo: ' + utils.o2s(nudo));
-				if (nudo.id0I == nudox.id0I) rowComun++;
-				if (nudo.id0F == nudox.id0F) colComun++;
-			})
-
-			if (onOff && colComun == 1) {
-				var col = this.getNodoById(nudox.id0F);
-				this.borraNodo(col);
-			}
-			if (onOff && rowComun == 1) {
-				var row = this.getNodoById(nudox.id0I);
-				this.borraNodo(row);
-			}
-
-			this.borraNodo(nudox);
-		}
-	}
-
-	getNudos(){
-		var nudos = [];
-		var nodoNudos = this.getNodoByRol('NUDOS');
-		if (nodoNudos && nodoNudos.hijos.length) nudos = this.getHijosNodo(nodoNudos);
-		return nudos;
-	}
-
-	getNudoByIds(id0,id1){
-		var nudos = this.getNudos();
-		var nudox = null;
-		nudos.map(function(nudo){
-			var ok = (nudo.id0I == id0) && (nudo.id0F == id1);
-			if (ok) nudox = nudo;
-		})
-		return nudox;
-	}
-
-	getMatrizVue(onOff){
-		var cols = this.getNodosCols();
-		var rows = this.getNodosRows();
-
-		var filas = [];
-		var raiz = this.getRaiz();
-		var fila = [raiz.tag]; // celda 0,0 de la tabla
-
-		for (var x=0;x<cols.length;x++){
-			fila.push(cols[x].tag); // celdas 0,x --> thead
-		}
-		filas.push(fila);
-
-		for (var y=0;y<rows.length;y++){
-			fila = [rows[y].tag]; // celda y,0 con el tag de cada fila0
-			for (var x=0;x<cols.length;x++){
-				var nudo = this.getNudoByIds(rows[y].id0,cols[x].id0)
-
-				     if ( nudo &&  onOff) fila.push('<i class="fa fa-check"></i>');
-				else if ( nudo && !onOff) fila.push(nudo.tag); // 
-				else if (!nudo &&  onOff) fila.push('<i class="fa fa-times"></i>');
-				else fila.push(' ');
-			}
-			filas.push(fila);
-		}
-
-		return filas;
-	}
-}
 
 export default {
-	rMeta,
-	rNodo,rDrag,rNudo,rArco,
-	rTopol,rConjt,rLista,rArbol,rGrafo,
-	rMalla,rMallaTree
+	rMeta,rTopol,
+	rNodo,rArco,rDrag,rARCO,
+	rConjt,rLista,rArbol,rGrafo,rMalla
 };
